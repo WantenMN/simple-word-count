16a6aba9dc0f86067bbedd32dec3b3f2ae786ec4
Wanten <WantenMN@gmail.com>
2024-07-30 11:24:55 +0800

    v1.0.0

--- main.ts
+++ main.ts
@@ -1,134 +1,150 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FileStatus {
+  name: string;
+  count: number;
+  checkTime: number;
+  size: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Data {
+  count: number;
+  initCount: number;
+  initTime: number;
+  files: FileStatus[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_DATA: Data = {
+  count: 0,
+  initCount: 0,
+  initTime: 0,
+  files: [],
+};
 
-	async onload() {
-		await this.loadSettings();
+export default class WordCountPlugin extends Plugin {
+  settings: Data;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  async onload() {
+    await this.loadSettings();
+    await this.processFiles();
+    this.registerEvent(
+      this.app.vault.on("modify", (file: TFile) => {
+        if (file.extension === "md") {
+          this.processFiles();
+        }
+      })
+    );
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_DATA, await this.loadData());
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  async countChineseCharacters(file: TFile): Promise<number> {
+    const content = await this.app.vault.read(file);
+    let count = 0;
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    for (const char of content) {
+      const codePoint = char.codePointAt(0);
+      if (codePoint && codePoint >= 0x4e00 && codePoint <= 0x9fa5) {
+        count++;
+      }
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    return count;
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  async processFiles() {
+    const now = Date.now();
+    const isSameDay = this.isSameDay(now, this.settings.initTime);
 
-	onunload() {
+    if (!isSameDay) {
+      this.settings.initTime = now;
+    }
 
-	}
+    const files = this.app.vault.getFiles();
+    const newFiles = await this.categorizeFiles(files, isSameDay);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    this.updateSettings(newFiles, isSameDay);
+    await this.saveSettings();
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+  async categorizeFiles(
+    files: TFile[],
+    isSameDay: boolean
+  ): Promise<FileStatus[]> {
+    const newFiles: FileStatus[] = [];
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    for (const file of files) {
+      if (isSameDay) {
+        await this.handleSameDayFile(file, newFiles);
+      } else {
+        await this.handleNewDayFile(file, newFiles);
+      }
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    return newFiles;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  async handleSameDayFile(file: TFile, newFiles: FileStatus[]) {
+    const existingFile = this.settings.files.find((f) => f.name === file.path);
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    if (existingFile) {
+      if (this.isFileModified(file, existingFile)) {
+        newFiles.push(await this.createFileStatus(file));
+      } else {
+        newFiles.push(existingFile);
+      }
+    } else {
+      newFiles.push(await this.createFileStatus(file));
+    }
+  }
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  async handleNewDayFile(file: TFile, newUnchanged: FileStatus[]) {
+    newUnchanged.push(await this.createFileStatus(file));
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  async createFileStatus(file: TFile): Promise<FileStatus> {
+    const count = await this.countChineseCharacters(file);
+    return {
+      name: file.path,
+      count,
+      checkTime: Date.now(),
+      size: file.stat.size,
+    };
+  }
 
-		containerEl.empty();
+  isFileModified(file: TFile, existingStatus: FileStatus): boolean {
+    return (
+      file.stat.mtime > existingStatus.checkTime ||
+      file.stat.size !== existingStatus.size
+    );
+  }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  updateSettings(newFiles: FileStatus[], isSameDay: boolean) {
+    this.settings.files = newFiles;
+    this.settings.count = this.calculateTotalCount(newFiles);
+
+    if (!isSameDay) {
+      this.settings.initCount = this.settings.count;
+    }
+  }
+
+  calculateTotalCount(files: FileStatus[]): number {
+    return files.reduce((sum, file) => sum + file.count, 0);
+  }
+
+  isSameDay(date1: number, date2: number): boolean {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return (
+      d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate()
+    );
+  }
 }
